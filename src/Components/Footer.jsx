c27f9011d4eb7aa3ad16f62ef046d19fc81d7a50
AshokRai <bono.ashok@gmail.com>
2023-12-19 05:47:13 +0545

    margin between pages adjusted

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight, faLocationDot, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 
 import {
@@ -50,58 +50,7 @@ function Footer() {
 						/>
 					</div>
 				</div>
-				<div className="footer-lower">
-					<div className="footer-nav">
-						<h2 className="footer-nav-title">Menu</h2>
-						<p className="footer-nav-item">Home</p>
-						<p className="footer-nav-item">About</p>
-						<p className="footer-nav-item">Blog</p>
-						<p className="footer-nav-item">Shop</p>
-					</div>
-					<div className="footer-instagram">
-						<h2 className="footer-nav-title">Instagram</h2>
-						<div className="footer-instagram-posts">
-							<div className="footer-instagram-post">
-								<img
-									className="footer-instagram-post-img"
-									src="/images/p1.png"
-									alt="p1"
-								/>
-								<p className="footer-intagram-post-description">
-									long established fact that a reader
-								</p>
-							</div>
-							<div className="footer-instagram-post">
-								<img
-									className="footer-instagram-post-img"
-									src="/images/p1.png"
-									alt="p2"
-								/>
-								<p className="footer-intagram-post-description">
-									long established fact that a reader
-								</p>
-							</div>
-						</div>
-					</div>
-					<div className="footer-contact">
-						<h2 className="footer-nav-title">Contact</h2>
-
-						<div className="footer-contact-info	">
-						<FontAwesomeIcon icon={faLocationDot} className="footer-contact-icons" />
-						<p>Location</p>
-						</div>
-						<div className="footer-contact-info	">
-						<FontAwesomeIcon icon={faPhone} className="footer-contact-icons"/>
-							<p>Location</p>
-						</div>
-						<div className="footer-contact-info	">
-							<FontAwesomeIcon
-								icon={faEnvelope} className="footer-contact-icons"
-							/>
-							<p>Location</p>
-						</div>
-					</div>
-				</div>
+				
 			</div>
 			<div className="footer-copyright">
 				<p>
--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -5,17 +5,22 @@ import "./Testimonials.css";
 function Testimonials() {
 	return (
 		<div className="testimonial-container">
+			
 			<h2 className="testimonial-heading">Testimonials</h2>
+
 			<div className="testimony-container">
 				<div className="client-image-contianer">
 					<img className="client-image" src="/images/client.jpg" alt="client" />
 				</div>
+				<div className="testimonial-sub-container">
 				<h6 className="client-name">Amit Singh</h6>
 				<p className="client-testimony">
 					It is a long established fact that a reader will be distracted by the
 					readable cIt is a long established fact that a reader will be
-					distracted by the readable c{" "}
+					distracted by the readable.This is not main content
 				</p>
+			</div>
+
 			</div>
 			<div className="slider">
 				<FontAwesomeIcon
